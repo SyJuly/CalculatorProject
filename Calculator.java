16fa09892ab0fc96006f6d2e47dbaeb3582a5954
Juliane Fink <jule-fink@gmx.de>
2016-10-16 19:23:39 +0200

    Added calculator class construction by David J. Barnes and Michael Kolling

---
+++ Calculator.java
@@ -0,0 +1,40 @@
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ * The main class of a simple calculator. Create one of these and you'll
+ * get the calculator on screen.
+ * 
+ * @author David J. Barnes and Michael Kolling
+ * @version 2008.03.30
+ */
+public class Calculator
+{
+    private CalcEngine engine;
+    private UserInterface gui;
+
+    /**
+     * Create a new calculator and show it.
+     */
+    public Calculator()
+    {
+        engine = new CalcEngine();
+        gui = new UserInterface(engine);
+    }
+
+    /**
+     * In case the window was closed, show it again.
+     */
+    public void show()
+    {
+        gui.setVisible(true);
+    }
+    
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException{
+    	
+    	UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    	
+    	Calculator calc = new Calculator();
+    	calc.show();
+    }
+}
